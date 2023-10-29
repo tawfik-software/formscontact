a7af56512ff55bedd1ccc0d2b079777768af5edc
tawfik-software <tawfik.ayade@yahoo.com>
2023-10-29 23:23:55 +0100

    add-api-email-and-alert

--- formscontact/contact.js
+++ formscontact/contact.js
@@ -1,3 +1,10 @@
-function validate () {
-    alert("sent with success")
+function sendMail(){
+    let parms = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        subject : document.getElementById('subject').value,
+        message : document.getElementById('message').value,
+    }
+
+    emailjs.send("form","template_wpgl2fj",parms).then(alert('email has been sent'))
 }
